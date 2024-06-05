fbeab8301dd7ddffe4e51d78fd257db47f87c744
山风 <hewenbao@yishouapp.com>
2024-06-05 23:45:14 +0800

    更改文件夹名, 方便按步骤学习

---
+++ src/main/java/com/atguigu/wc_1/baozitest.java
@@ -0,0 +1,39 @@
+package com.atguigu.wc_1;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class baozitest {
+    public static void main(String[] args) throws Exception {
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
+        env.setParallelism(3);
+
+        DataStreamSource<String> source = env.socketTextStream("hadoop102", 7777);
+
+        source.flatMap(
+                (String value, Collector<Tuple2<String, Integer>> out) -> {
+                    for (String word : value.split(" ")) {
+                        out.collect(Tuple2.of(word, 1));
+                    }
+                }
+        )
+                .returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(new KeySelector<Tuple2<String, Integer>, String>() {
+                    @Override
+                    public String getKey(Tuple2<String, Integer> stringIntegerTuple2) throws Exception {
+                        return stringIntegerTuple2.f0;
+                    }
+                })
+                .sum(1)
+                .print();
+
+        env.execute();
+
+    }
+}
